5725d719c1e19b165b1b1f53a8fa0f97d1f04ebd
Chelsea Hilditch <git@cdhildit.ch>
2020-07-19 11:27:15 +0100

    Add Point class
    Replace all coordinate usage with Point class

---
+++ src/tictactoe/Point.java
@@ -0,0 +1,20 @@
+package tictactoe;
+
+public class Point {
+    public int x, y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+}
